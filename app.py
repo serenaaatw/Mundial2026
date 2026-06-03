099d82ab1d3bb422c497e7fc65544c8daaf6719c
serena06 <serenavargas@escuelathays.edu.ar>
2026-06-03 19:26:36 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from models.db import db
+from config.config import DATABASE_CONNECTION_URI
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#app.register_blueprint()
+
+db.init_app(app)
+
+with app.app_context():
+    #from models.  import
+    #db.drop_all()
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
