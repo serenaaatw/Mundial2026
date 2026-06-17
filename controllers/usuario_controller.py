1f144adde68964ecaaae876d5bd24b0819d36405
Tomas <tomymanijacaballero@gmail.com>
2026-06-16 22:36:12 -0300

    implementacion de diseño responsive

--- controllers/usuario_controller.py
+++ controllers/usuario_controller.py
@@ -1,39 +1,42 @@
 from models.db import db
 from models.usuario import Usuario
-from flask import jsonify 
 import re
 import os
 from werkzeug.utils import secure_filename
+
 def ver_usuario(id):
     usuario = Usuario.query.get(id)
     if usuario:
         return usuario
     return {"ERROR": "El usuario no existe"}
 
-def actualizar_perfil(id, data, foto = None):
+def actualizar_perfil(id, data, foto=None):
     user = Usuario.query.get(id)
     tipos = {
-        "nombre_usuario":str ,
-        "mail_usuario":str ,
-        "foto_perfil":str
+        "nombre_usuario": str,
+        "mail_usuario": str,
+        "foto_perfil": str
     }
     if user:
-
         if foto and foto.filename != "":
-
             carpeta = "static/uploads"
-
             if not os.path.exists(carpeta):
                 os.makedirs(carpeta)
             nombre_imagen = secure_filename(foto.filename)
-            ruta = os.path.join(carpeta,nombre_imagen)
+            ruta = os.path.join(carpeta, nombre_imagen)
+            if os.path.exists(ruta):
+                raise ValueError(
+                    "Ya existe una imagen con ese nombre, elegí otra"
+                )
             foto.save(ruta)
-            user.foto_perfil = ("uploads/" + nombre_imagen)
+            user.foto_perfil = "uploads/" + nombre_imagen
         for key, value in data.items():
             if key in tipos:
                 value = tipos[key](value)
             if key == "mail_usuario":
+
                 patron = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+
                 if not re.match(patron, value):
                     raise ValueError(
                         "El correo ingresado no es válido"
