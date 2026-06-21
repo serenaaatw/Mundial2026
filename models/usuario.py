560cf8611f62c8739e5ffa16a435fff068cbdb71
Uriel Martinez <martinezuri20@gmail.com>
2026-06-20 22:59:42 -0300

    arreglos y modificación en la verificación de correos y contraseñas

--- models/usuario.py
+++ models/usuario.py
@@ -7,25 +7,22 @@ class Usuario(db.Model):
     email=db.Column(db.String(200),unique=True, nullable=False)
     password=db.Column(db.String(300), nullable=False)
     rol=db.Column(db.String(20), nullable=False)
-    foto_perfil=db.Column(db.String(400),default="img/usuario/foto_usuario.png")
     #polomorfismo de herencia para identificar en el programa que objeto crear teniendo en cienta el atributo rol
     __mapper_args__={"polymorphic_on":rol,
                      "polymorphic_identity":"usuario"}
     
-    def __init__(self,nombre,email,password,rol,foto_perfil):
+    def __init__(self,nombre,email,password,rol):
         self.nombre=nombre
         self.email=email
         self.password=password
         self.rol=rol
-        self.foto_perfil=foto_perfil
     def serialize(self):
         return {
             "id":self.id,
             "nombre":self.nombre,
             "email":self.email,
             "password":self.password,
-            "rol":self.rol,
-            "foto perfil":self.foto_perfil
+            "rol":self.rol
         }
     #encapsulamiento de contraseña.
 
